92a53d4f51ea739185f16252ebc108cdbde4c989
Lattapon <lattapon.mu@gmail.com>
2025-11-26 10:24:53 +0700

    test1111

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('test123456! Hello from CI/CD Web! Build: ' + (process.env.BUILD_ID || 'local'));
+  res.end('test1234568! Hello from CI/CD Web! Build: ' + (process.env.BUILD_ID || 'local'));
 });
 
 server.listen(port, () => console.log('Server running on port', port));
